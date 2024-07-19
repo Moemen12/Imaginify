b1bc8ea2158c07a98a36ae8c385b842c511730a4
moemen saadeh <moemen.gcbdev@gulfofficial.com>
2024-07-20 00:27:54 +0300

    first commit

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,39 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureUrl: URL;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl: URL;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author?: Schema.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const ImageSchema = new Schema<IImage>({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { type: String, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: String, required: true },
+  aspectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const Image = models.Image || model<IImage>("Image", ImageSchema);
+
+export default Image;
